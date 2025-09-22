1449e8b2265a2dc2b3440c0bafbe0a615d3531f6
Fintan Healy <fintan.healy@bristol.ac.uk>
2025-09-22 23:12:24 +0100

    more comments

--- Worksheet2/problem_3.m
+++ Worksheet2/problem_3.m
@@ -1,7 +1,3 @@
-%% The Task: Write a MATLAB script to estimate the value of pi:
-%1. Ensure the script stops when the estimate reaches a suitable degree of 
-% accuracy (the error between the estimate and real value is within 
-% a desired tolerance, perhaps 0.1%) HINT: use a while loop
-% 2. Plot a log-scale graph of the error against the number of darts thrown
+%% Task 1
 
     
